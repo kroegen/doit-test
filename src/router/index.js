24cdcfc7501637dbc3554939670153628518f214
Yevhenii Krotenko <kroegen4@gmail.com>
2019-08-19 00:33:18 +0300

    basic styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue        from 'vue';
 import Router     from 'vue-router';
+import store      from '@/store';
 import LoginPage     from '@/views/Login';
 import LandingPage   from '@/views/Landing';
 
@@ -12,13 +13,27 @@ const router = new Router({
             path      : '/',
             name      : 'landing',
             component : LandingPage,
+            meta      : { header: true, auth: true },
         },
         {
             path      : '/login',
             name      : 'login',
             component : LoginPage,
+            meta      : { header: false, auth: false },
         },
     ]
+});
+
+router.beforeEach((to, from, next) => {
+    const isLogged = store.state.auth.isLogged;
+
+    if (to.meta.auth && !isLogged) {
+        next({ path: '/login' });
+    } else if (to.name === 'login' && isLogged) {
+        next(false);
+    } else {
+        next();
+    }
 });
 
 export default router;
