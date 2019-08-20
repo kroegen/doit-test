c8c40c57b959931910f2acdacce986f120f06ffb
Yevhenii Krotenko <kroegen4@gmail.com>
2019-08-20 22:30:23 +0300

    not found page + wc

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="wrapper">
+        <h1 class="title is-1">Page not found</h1>
+        <a href="/">try again</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'notfound',
+};
+</script>
